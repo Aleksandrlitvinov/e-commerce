840a000cc8da435eaf44a521cb4e69f495e7e09b
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-04-15 22:39:51 +0200

    feat: fetch portion of data

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,12 +1,18 @@
 import { ProductsSection } from '@/features'
+import { useGetCategoriesQuery, useGetProductsQuery } from '@/shared'
 
 export const HomePage = () => {
+  const { data: categoriesList } = useGetCategoriesQuery()
+  const { data: productsList } = useGetProductsQuery()
+  const categories = categoriesList?.slice(0, 6)
+  const products = productsList?.products?.slice(5, 8)
+
   return (
     <div>
       <ProductsSection
         buttonTitle={'More Products'}
-        categories={[]}
-        products={[]}
+        categories={categories}
+        products={products}
         title={'Best selling products'}
       />
     </div>
